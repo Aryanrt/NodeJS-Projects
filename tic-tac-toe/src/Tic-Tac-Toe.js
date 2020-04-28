36bdcf57cd00ef7f356231f860b1090b8fdf9ba4
aryanrt <aryan.r.t@hotmail.com>
2020-04-28 16:39:18 -0400

    X-O Turn was added

--- tic-tac-toe/src/Tic-Tac-Toe.js
+++ tic-tac-toe/src/Tic-Tac-Toe.js
@@ -5,56 +5,68 @@ import './Tic-Tac-Toe.css';
 
 class Square extends React.Component {
   
-constructor(props) {
+/*constructor(props) {
     super(props);
     this.state = {
       value: null,
     };
-  }
+  }*/
 	render() {
     return (
-     /*<button className="square" onClick={function() { alert('click'); }}>  OR */
-      <td className="square" onClick={() => this.setState({value: this.props.value})}>
-        {this.state.value}
+     /*<button className="square" onClick={function() { this.props.onClick(); }}>  OR */
+      <td className="square" onClick={() => this.props.onClick()}>
+		
+        {this.props.value}
       </td>
     );
   }
 }
 
 class Board extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+     xNext: true,
+	 squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i)
+  {
+	//square already pressed!!!
+	if( this.state.squares[i]==='X' ||this.state.squares[i]==='O' )
+		return;
+		
+	this.state.squares[i] = this.state.xNext ? 'X' : 'O'; 
+	this.setState({squares: this.state.squares, xNext: ! this.state.xNext,});  
+  }
   renderSquare(i) {
-    return <Square value={i}/>;
+   // return <Square value={i}/>;
+  return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: '+( this.state.xNext? 'X': 'O');
 
     return (
       <div>
         <div className="status">{status}</div>
 		<table className="buttons">
-			<tr>
-		        
+			<tr >		        
 		          {this.renderSquare(0)}
 		          {this.renderSquare(1)}
-		          {this.renderSquare(2)}
-		        
+		          {this.renderSquare(2)}		        
 			</tr>
-			<tr>
-		        
+			<tr>		        
 		          {this.renderSquare(3)}
 		          {this.renderSquare(4)}
-		          {this.renderSquare(5)}
-		        
+		          {this.renderSquare(5)}		        
 			</tr>
-			<tr>
-		        
+			<tr>		        
 		          {this.renderSquare(6)}
 		          {this.renderSquare(7)}
-		          {this.renderSquare(8)}
-		        
-			</tr>
-			
+		          {this.renderSquare(8)}		        
+			</tr>			
 		</table>
       </div>
     );
