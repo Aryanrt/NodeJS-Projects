8373ee414389d10b3b32179c9dce8cdbaec1ea54
aryanrt <aryan.r.t@hotmail.com>
2020-04-30 00:23:43 -0400

    Classes as components

--- tests/src/Test.js
+++ tests/src/Test.js
@@ -10,10 +10,46 @@ function GuestGreeting(props) {
 
 function Greeting(props) {
   const isLoggedIn = props.isLoggedIn;
+  let b = <SignUpButton parameter={isLoggedIn}  />;
   if (isLoggedIn) {
-    return <UserGreeting />;
+    return <div><UserGreeting />{b}</div>;
   }
-  return <GuestGreeting />;
+  return <div><GuestGreeting />{b}</div>;
+}
+class SignUpButton extends React.Component
+{
+	constructor(props) 
+	{
+	  super(props);
+      this.state = {par: this.props.parameter};
+	  //this.foo = this.foo.bind(this);
+	  this.signIn = this.signIn.bind(this);
+	  this.signOut = this.signOut.bind(this);
+	}
+	foo(props)
+	{
+		if( ! this.state.par)
+			return <button onClick={this.signIn}>Sign Up</button>;
+		return;
+		
+	}
+	signIn()
+	{
+		this.setState({par: true});
+	}
+	signOut()
+	{
+		this.setState({par: false});
+	}
+	render()
+	{
+	 //let x = this.foo();
+	 //return (<div>{x}</div>);
+		if( ! this.state.par)
+			return <button onClick={this.signIn}>Sign In</button>;
+		else
+			return <button onClick={this.signOut}>Sign out</button>;
+	}
 }
 
 export default Greeting;
